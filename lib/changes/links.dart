d9b56f3a4132e8ac478e113e4970f962c010ba3a
MhdMihdi <mhdmihdi2@gmail.com>
2024-11-05 20:23:30 +0300

    upload files

---
+++ lib/changes/links.dart
@@ -0,0 +1,16 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1HRWfrlZPxOOM_yRlACeOF4E77DZCu4Ml/view?usp=drivesdk';
+
+// Github Link
+const String gitHub = 'https://github.com/MhdMihdi';
+//gitlab
+const String gitlab = 'https://gitlab.com/mohammadmihdi';
+// facebook Link
+const String facebook = 'https://facebook.com/mohammad.mihdi.902';
+// linkedin Link
+const String linkedin = 'https://linkedin.com/in/mohammad-mihdi-490001289';
+// instagram Link
+const String instagram = 'https://instagram.com/mhd_mihdi';
+// Whatsapp Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=963992281939';
---
+++ lib/changes/strings.dart
@@ -0,0 +1,27 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Mohammad Mihdi,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Developer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+//
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Mohammad Mihdi, a Mobile App developer.';
+
+const String aboutMeDetail =
+    'I\'m studing Software Engineering in Damascus University, Syria. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience.';
